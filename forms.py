2e629f9cee8427e3b6016ece7d6ec2cd7fd37e6b
theopmw <theopmw@gmail.com>
2021-07-06 07:53:43 +0000

    Install Flask-WTF and create forms.py file

---
+++ forms.py
@@ -0,0 +1,5 @@
+from flask_wtf import FlaskForm
+from wtforms import (
+    StringField, PasswordField,
+    BooleanField, SubmitField, TextAreaField)
+from wtforms.validators import DataRequired, Length, EqualTo, Email
