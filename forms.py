5d76a3ba202f527333ce19a1a66868eb28d131ee
theopmw <theowrightdev@gmail.com>
2021-07-20 08:04:34 +0000

    Add AddRecipeForm to forms.py

--- forms.py
+++ forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import (
     StringField, PasswordField,
-    BooleanField, SubmitField, TextAreaField)
+    BooleanField, SelectField, IntegerField,
+    SubmitField, TextAreaField)
 from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 
@@ -22,3 +23,24 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign In')
+
+
+class AddRecipeForm(FlaskForm):
+    recipe_name = StringField('Recipe Name', validators=[DataRequired()])
+    recipe_description = TextAreaField(
+        'Recipe Description', validators=[DataRequired()])
+    prep_time = IntegerField('Prep Time', validators=[DataRequired()])
+    cook_time = IntegerField('Cook Time', validators=[DataRequired()])
+    serves = SelectField(
+        'Servings (choose an option)', choices=[(2), (4), (6), (8), (10)],
+        validate_choice=True, validators=[DataRequired()])
+    difficulty = SelectField(
+        'Difficulty (choose an option)',
+        choices=[('Easy'), ('Medium'), ('Hard')], validators=[DataRequired()])
+    tags = StringField(
+        'Tags (separate each with a comma', validators=[DataRequired()])
+    image = StringField('Image link', validators=[DataRequired()])
+    ingredients = TextAreaField(
+        'Ingredients (one per line)', validators=[DataRequired()])
+    method = TextAreaField('Method', validators=[DataRequired()])
+
