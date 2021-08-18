ba2bae9da60757cf341d6a10e087a6d62f424ffc
theopmw <theowrightdev@gmail.com>
2021-08-18 11:48:35 +0000

    Switch debug to False

--- app.py
+++ app.py
@@ -307,4 +307,4 @@ def handle_500(error):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
